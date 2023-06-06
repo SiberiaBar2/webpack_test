a86ee6346e898c608d0d17a2c2c3c4ca95f28f7f
karlfranz <2031665973@qq.com>
2023-06-06 14:19:51 +0800

    处理其他资源的配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import count from "./js/count";
 import sum from "./js/sum";
 
+// 注意:要想让webpack打包资源，必须引入该资源
+import "./css/iconfont.css";
+import "./css/index.css";
+import "./less/index.less";
+import "./scss/index.scss";
+import "./scss/index.sass";
+import "./stylus/index.styl";
+
 console.log(count(2, 1));
 console.log(sum(1, 2, 3, 4));
- 
